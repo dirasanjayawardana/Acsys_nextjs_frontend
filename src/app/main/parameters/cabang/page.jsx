e93fe7c86066ebf4c4bed1990f2fa5642e106ea1
dirasanjayawardana <dirawardana.dw@gmail.com>
2024-05-22 17:03:42 +0700

    add: search by name or kode in cabang dan produk

--- src/app/main/parameters/cabang/page.jsx
+++ src/app/main/parameters/cabang/page.jsx
@@ -1,14 +1,17 @@
 "use client";
 import { dummyBodyTable, dummyHeaderTable } from "@/assets/dummyData";
 import FormSearch from "@/components/FormSearch";
+import NotFound from "@/components/NotFound";
 import PleaseWait from "@/components/PleaseWait";
 import Table from "@/components/Table";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 
 const Page = () => {
-    const [serachInput, setSearchInput] = useState("");
+    const [searchInput, setSearchInput] = useState("");
     const [dataCabang, setDataCabang] = useState(null);
+    const [searchResult, setSearchResult] = useState(null);
+
     const [currentPage, setCurrentPage] = useState(1);
     const [startIndex, setStartIndex] = useState(0);
     const [perPage, setPerPage] = useState(10);
@@ -29,15 +32,27 @@ const Page = () => {
         }
     };
 
+    const handleSearch = async () => {
+        try {
+            const response = await axios.get(
+                `${process.env.NEXT_PUBLIC_ACSYS_URL_SERVER}/cabang/getcabang?input=${searchInput}`
+            );
+            setSearchResult(response.data.data);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
     return (
         <div>
             <div className="flex justify-between items-center mt-5">
                 <FormSearch
                     placeholder="Cari Cabang"
                     setState={setSearchInput}
+                    handleSubmit={handleSearch}
                 />
 
-                {dataCabang && (
+                {dataCabang && !searchResult && (
                     <div className="w-full flex justify-end items-center gap-3">
                         <button
                             disabled={currentPage === 1 || startIndex === 0}
@@ -51,7 +66,9 @@ const Page = () => {
                         </button>
                         <h5 className="font-semibold">{currentPage}</h5>
                         <button
-                            disabled={(startIndex + perPage) >= dataCabang[0].maxSize}
+                            disabled={
+                                startIndex + perPage >= dataCabang[0].maxSize
+                            }
                             onClick={() => {
                                 setCurrentPage(currentPage + 1);
                                 setStartIndex(startIndex + 10);
@@ -64,16 +81,36 @@ const Page = () => {
                 )}
             </div>
 
-            {dataCabang ? (
+            {dataCabang && (!searchResult || searchInput == "") ? (
                 <div className="mt-5">
                     <Table
-                        headers={Object.keys(dataCabang[0]).slice(0,Object.keys(dataCabang[0]).length - 1)}
+                        headers={Object.keys(dataCabang[0]).slice(
+                            0,
+                            Object.keys(dataCabang[0]).length - 1
+                        )}
                         data={dataCabang}
                         action={true}
                     />
                 </div>
             ) : (
-                <PleaseWait />
+                !(searchResult && searchInput != "") && <PleaseWait />
+            )}
+
+            {searchResult && searchInput != "" && searchResult.length !== 0 && (
+                <div className="mt-5">
+                    <Table
+                        headers={Object.keys(searchResult[0]).slice(
+                            0,
+                            Object.keys(searchResult[0]).length - 1
+                        )}
+                        data={searchResult}
+                        action={true}
+                    />
+                </div>
+            )}
+
+            {searchResult && searchInput != "" && searchResult.length === 0 && (
+                <NotFound />
             )}
         </div>
     );
