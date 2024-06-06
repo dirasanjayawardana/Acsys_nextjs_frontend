af9a77d9b5c6af0ef5fcec9f9c2a2d2d20d93e4b
DitaRizkiWD <rdita201@gmail.com>
2024-06-06 08:47:50 +0700

    add: user list but error

--- src/app/main/administrator/userlist/page.jsx
+++ src/app/main/administrator/userlist/page.jsx
@@ -1,16 +1,40 @@
+"use client"
 import FormSearch from "@/components/userlist/FormSearch";
 import TableUser from "@/components/userlist/TableUser";
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
-const page = () => {
+const Page = () => {
+    const [dataUser, setDatauser] = useState("")
+
+    useEffect(() => {
+        getDataUser();
+
+    }, [])
+
+    const getDataUser = async() => {
+        setDatauser(null);
+        try {
+            const response = await axios.get(
+                `${process.env.NEXT_PUBLIC_ACSYS_URL_SERVER}/secure/useracsys`
+            );
+            setDatauser(response.data.data)
+            // console.log(response.data.data)
+        } catch (error) {
+            console.log(error)
+        }
+    }
     return (
         <div className="flex flex-col">
             <FormSearch />
             <div className="w-full mt-5">
-                <TableUser />
+                {/* <TableUser 
+                    headers={Object.keys(dataUser[0])}
+                    data={dataUser}
+                /> */}
             </div>
         </div>
     );
 };
 
-export default page;
+export default Page;
--- src/components/userlist/TableUser.jsx
+++ src/components/userlist/TableUser.jsx
@@ -1,79 +1,70 @@
 "use client";
-import React, { useState } from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import { FiEdit, FiTrash2 } from "react-icons/fi";
 
-const TableUser = () => {
-    const data = Array.from({ length: 25 }, (_, index) => index + 1);
+const TableUser = ({headers, data}) => {
+    
+    // const data = Array.from({ length: 25 }, (_, index) => index + 1);
 
-    const [currentPage, setCurrentPage] = useState(1);
-    const [perPage, setPerPage] = useState(10);
+    // const [currentPage, setCurrentPage] = useState(1);
+    // const [perPage, setPerPage] = useState(10);
 
-    const paginateData = () => {
-        const lastIndex = currentPage * perPage;
-        const firstIndex = lastIndex - perPage;
-        return data.slice(firstIndex, lastIndex);
-    };
+    // const paginateData = () => {
+    //     const lastIndex = currentPage * perPage;
+    //     const firstIndex = lastIndex - perPage;
+    //     return data.slice(firstIndex, lastIndex);
+    // };
+    // const [dataUser, setDatauser] = useState("")
+
+    // useEffect(() => {
+    //     getDataUser();
+
+    // }, [])
+
+    // const getDataUser = async() => {
+    //     setDatauser(null);
+    //     try {
+    //         const response = await axios.get(
+    //             `${process.env.NEXT_PUBLIC_ACSYS_URL_SERVER}/secure/useracsys`
+    //         );
+    //         console.log(response.data.data);
+    //     } catch (error) {
+            
+    //     }
+    // }
 
     return ( 
-        <div>
-            <table className="w-full text-center">
+        <div className="overflow-auto mx-auto">
+            <table className="text-center border-b cursor-pointer">
                 <thead>
-                    <tr className="border-b-2 bg-blue-300">
-                        <th className="py-2 px-4">No</th>
-                        <th className="py-2 px-4">Username</th>
-                        <th className="py-2 px-4">Email</th>
-                        <th className="py-2 px-4">Role</th>
-                        <th className="py-2 px-4">Created At</th>
-                        <th className="py-2 px-4">Updated At</th>
-                        <th className="py-2 px-4">Status</th>
-                        <th className="py-2 px-4">Action</th>
+                    <tr className="border-b-2 bg-blue-300 text-sm">
+                        {headers.map((item, index) => (
+                            <th key={index} className="py-3 px-6 capitalize">
+                                {item}
+                            </th>
+                        ))}
                     </tr>
                 </thead>
-                <tbody>
-                    {paginateData(data).map((item, index) => (
+                <tbody className="bg-black">
+                    {data.map((item, index) => (
                         <tr
                             key={index}
                             className={`${
                                 index % 2 === 0 ? "bg-white" : "bg-blue-100"
-                            } hover:bg-gray-100`}
+                            } hover:bg-gray-100 text-xs leading-5`}
                         >
-                            <td className="py-2 px-4">{item}</td>
-                            <td className="py-2 px-4">value{item}</td>
-                            <td className="py-2 px-4">value{item}</td>
-                            <td className="py-2 px-4">value{item}</td>
-                            <td className="py-2 px-4">value{item}</td>
-                            <td className="py-2 px-4">value{item}</td>
-                            <td className="py-2 px-4">value{item}</td>
-                            <td className="py-2 px-4 flex items-center justify-center gap-3">
-                                <button className="text-red-500">
-                                    <FiTrash2 size={20} />
-                                </button>
-                                <button className="text-yellow-600">
-                                    <FiEdit size={20} />
-                                </button>
-                            </td>
+                            {headers.map((header, headerIndex) => (
+                                <td key={headerIndex} className="py-3 px-6">
+                                    {item[header]}
+                                </td>
+                            ))}
                         </tr>
                     ))}
-                </tbody>
-            </table>
-
-            <div className="mt-3 w-full flex justify-center items-center gap-3">
-                <button
-                    disabled={currentPage === 1}
-                    onClick={() => setCurrentPage(currentPage - 1)}
-                    className="py-2 px-4 rounded-xl bg-blue-500 text-white"
-                >
-                    Prev
-                </button>
-                <h5 className="font-semibold">{currentPage}</h5>
-                <button
-                    disabled={currentPage === Math.ceil(data.length / perPage)}
-                    onClick={() => setCurrentPage(currentPage + 1)}
-                    className="py-2 px-4 rounded-xl bg-blue-500 text-white"
-                >
-                    Next
-                </button>
-            </div>
+            </tbody>
+    </table>
+            
+            
         </div>
     );
 };
